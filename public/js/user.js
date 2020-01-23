33e98d155f461c27b564fcf6026ac1a2c7001bea
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2020-01-22 19:59:22 -0500

    updated html, css, and js

--- public/js/user.js
+++ public/js/user.js
@@ -17,3 +17,90 @@ function myFunction() {
       }
     }
   }
+
+  function catFunctionProtein() {
+    document.getElementById("proteinDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.drop-click')) {
+      let dropdowns = document.getElementsByClassName("category-dropdown");
+      let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  function catFunctionVeggie() {
+    document.getElementById("veggiesDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.drop-click')) {
+      let dropdowns = document.getElementsByClassName("category-dropdown");
+      let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+  function catFunctionCarb() {
+    document.getElementById("carbDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.drop-click')) {
+      let dropdowns = document.getElementsByClassName("category-dropdown");
+      let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+  function catFunctionDairy () {
+    document.getElementById("dairyDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.drop-click')) {
+      let dropdowns = document.getElementsByClassName("category-dropdown");
+      let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  $("p").on("click", function(event){
+    event.preventDefault();
+    console.log('retrieve product');
+
+    // for(let i=0; i< arguments.product.length; i++){
+    //   let newItem = a.product[i]
+    // }
+
+   console.log($("p"));
+
+    $("#myDropdown").append('<p> ' + $("#id") + '</p>')
+  });
